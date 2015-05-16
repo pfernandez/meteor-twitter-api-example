ae935372d1da6644c951881a532f1edd6f5fff68
Paul Fernandez <paul4nandez@gmail.com>
2015-05-15 19:25:59 -0700

    data on the front end

--- twitter-api.js
+++ twitter-api.js
@@ -1,50 +1,57 @@
+Tweets = new Mongo.Collection('tweets');
+
+
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
 
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
+	Meteor.subscribe('tweets');
+	
+	Template.twitterData.helpers({
+		rawData: function () {
+			return JSON.stringify(Tweets.findOne(), null, 2);
+		},
+		data: function () {
+			return Tweets.findOne();
+		}
+	});
 
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
-  });
 }
 
+
 if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-
-    var Twit = Meteor.npmRequire('twit');
-
-		// "api" should be an object containing Twitter access keys, i.e.
-		//	api = {
-		//		consumer_key:         'YOUR_API_KEY',
-		//		consumer_secret:      'YOUR_API_SECRET',
-		//		access_token:         'YOUR_ACCESS_TOKEN', 
-		//		access_token_secret:  'YOUR_TOKEN_SECRET'
-		//	}
+	Meteor.startup(function () {
 		
-    var T = new Twit(api);
+		// If our collection is empty, grab some data and insert it.
+		if(Tweets.find().count() < 1) { 
 
-    //  search twitter for all tweets containing the word 'banana'
-    //  since Nov. 11, 2011
-    T.get('search/tweets',
-        {
-            q: 'banana since:2011-11-11',
-            count: 100
-        },
-        function(err, data, response) {
-            if(! err && 200 == response.statusCode) {
-                console.log(data);
-            }
-        }
-    );
+			var Twit = Meteor.npmRequire('twit');
+			
+			/*
+			apiKeys = {
+				consumer_key:         'YOUR_API_KEY',
+				consumer_secret:      'YOUR_API_SECRET',
+				access_token:         'YOUR_ACCESS_TOKEN', 
+				access_token_secret:  'YOUR_TOKEN_SECRET'
+			}
+			*/
 
-  });
+			var T = new Twit(apiKeys);
+			T.get('search/tweets',
+				{
+					q: 'meteorjs',
+					count: 10
+				},
+				Meteor.bindEnvironment(function(err, data, response) {
+					if(! err && 200 == response.statusCode) {
+						Tweets.insert(data);
+					}
+				})
+			);
+		}
+		
+		// Make the data available to the client.
+		Meteor.publish('tweets', function () {
+		  return Tweets.find();
+		});
+		
+	});
 }
